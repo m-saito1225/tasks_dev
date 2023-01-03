fd6016b5762127240bd5824a78b4dfcb6b5d03d9
m-saito1225 <12251225s@gmail.com>
2023-01-03 17:58:07 +0900

    インデント調整

--- app/Consts/BookConsts.php
+++ app/Consts/BookConsts.php
@@ -6,7 +6,7 @@ class BookConsts
 	// タスクステータス
 	public const VERYBAD	= 1	;
 	public const BAD    	= 2	;
-	public const NORMAL		= 3 ;
+	public const NORMAL 	= 3 ;
 	public const GOOD   	= 4 ;
 	public const VERYGOOD	= 5 ;
 	
